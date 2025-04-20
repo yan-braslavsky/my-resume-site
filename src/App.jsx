9fe5e1adf0b6e801409f8faff96040884b30ce05
Yan Braslavsky <yan.braslavsky@gmail.com>
2025-04-20 15:10:46 +0200

    refactor: Add comment to clarify background section in App component

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
 
   return (
     <div className="app-container">
+      {/* Background */}
       <BlurredParallaxBackground />
       <SimpleDotBackground />
       <ParticlesBackground />
